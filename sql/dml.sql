a72a38374f859498de6222eef2e65444b486394f
Craudin14 <claudioanjos130681@gmail.com>
2025-10-29 12:02:01 -0300

    Update dml.sql

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,2 +1,4 @@
 INSERT INTO ALUNO (nome, data_nasc, cpf, telefone, email)
-VALUES ('Davi', 02/10/2002, 12345678925, 987456358, 'davi12@gmail.com');
+VALUES ('Davi', 02/10/2002, 12345678925, 987456358, 'davi12@gmail.com'),
+       ('Rodrigo', 06/10/2000, 78652987410, 965487412, 'rodrigo88@gmail.com'),
+       ('Pedro', 07/07/1999, 12978954678, 984563217, 'pedro@gmail.com'); 
