dc6654d77bbf953e45583c057817621b15747522
Craudin14 <claudioanjos130681@gmail.com>
2025-10-29 14:49:08 -0300

    Update dml.sql

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,4 +1,15 @@
 INSERT INTO ALUNO (nome, data_nasc, cpf, telefone, email)
 VALUES ('Davi', 02/10/2002, 12345678925, 987456358, 'davi12@gmail.com'),
        ('Rodrigo', 06/10/2000, 78652987410, 965487412, 'rodrigo88@gmail.com'),
-       ('Pedro', 07/07/1999, 12978954678, 984563217, 'pedro@gmail.com'); 
+       ('Pedro', 07/07/1999, 12978954678, 984563217, 'pedro@gmail.com');
+
+INSERT INTO PROFESSOR (nome, data_nasc, cpf, especialidade, telefone, email)
+VALUES ('Beatriz', 10/10/1992, 16598741236,'Hip Hop', 968745321, 'bia@gmail.com'),
+       ('Manuela', 05/09/1994, 98745632185, 'Balé', 9876563251, 'manu@gmail.com'),
+       ('Flora', 29/07/1989, 65478965871, 'Tango', 985664752, 'floris@gmail.com');
+
+INSERT INTO ESTILODANCA (nome, descricao)
+VALUES ('Hip Hop', 'Dança contemporânea'),
+       ('Balé', 'Dança clássica'),
+       ('Tango', 'Dança moderna');
+
